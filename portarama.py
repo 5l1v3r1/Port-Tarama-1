3482705fe06a35aed6d15e5b550fcf61e2322b61
Fırat Esatoğlu <60032785+firatesatoglu@users.noreply.github.com>
2020-11-10 21:03:34 +0300

    Update portarama.py

--- portarama.py
+++ portarama.py
@@ -2,10 +2,10 @@ import socket
 from datetime import datetime
 
 rServer = input("IP Adresini Giriniz: ")
-zamanAsımı = 3
+zamanAsımı = 3 #Saniye başına bağlantı(tarama)
 
 try:
-    for port in range(20,1001):
+    for port in range(20,1001): # Aralağında port taramsı yapar.
         baglantı = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         baglantı.settimeout(zamanAsımı)
         sonuc = baglantı.connect_ex((rServer, port))
